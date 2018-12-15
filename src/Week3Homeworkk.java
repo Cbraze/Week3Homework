723c2b5e1e771a202de52dc167f148dd54339688
Craig Brazelton <brazeschool@gmail.com>
2018-12-15 12:47:47 -0700

    week 3 homework

---
+++ src/Week3Homeworkk.java
@@ -0,0 +1,16 @@
+
+public class Week3Homeworkk {
+
+	public static void main(int[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		
+		
+		int[] intArray = new int[] {4,5,6,7,8};
+		for (int i=0; i<intArray.length; i++)
+		{
+		  System.out.println(intArray[i]);
+		}
+	}
+}
